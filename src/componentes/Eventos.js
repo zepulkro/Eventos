dfd9b3fccb5ac2e9ff8afb25208c44902c2f3be3
Jorge Seminario <jorge.seminario.dev@gmail.com>
2019-01-22 14:26:19 -0500

    first commit

---
+++ src/componentes/Eventos.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Evento from './Evento';
+import PropTypes from 'prop-types';
+
+class Eventos extends Component {
+    render() { 
+        return ( 
+            <div className="uk-child-width-1-3@m" uk-grid="ture">
+                { Object.keys(this.props.eventos).map(key => (
+                    <Evento
+                        key={key}
+                        info={this.props.eventos[key]}
+                    />
+                ))}
+            </div>
+         );
+    }
+}
+ 
+Eventos.propTypes = {
+    eventos: PropTypes.array.isRequired
+}
+
+export default Eventos;
